f01650a6cbe0947c7689745464a3a8dcc739f742
Yume Fernandes <107142833+Dehrangerz9@users.noreply.github.com>
2025-04-03 09:46:15 +0000

    Initial commit

---
+++ src/menu.tsx
@@ -0,0 +1,16 @@
+const Menu = () => {
+  return (
+    <>
+      <div className=" h-[90vh] w-[100vw]  rotating-menu perspective-distant">
+        <ul className="0 text-7xl text-black  translate-z-0 rotate-y-25 bg-white ">
+          <li>Skills</li>
+          <li>Projetos</li>
+          <li>Sobre mim</li>
+          <li>Contato</li>
+          <li>FAQ</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+export default Menu;
